b89bb6cf488348445caa154e47530466676cbddf
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-10-03 21:06:57 -0700

    Forked repo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+// Lab 10
+
 #include <iostream>
 using namespace std;
 
+// main(): Outputs to console a message confirming assignment
+// Arguments: None really.
+// Returns: Returns 0, as intended by the gods who came before us known as Thompson, Richie and Kernighan.
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    cout << "Forked gh repo of Lab10." << endl;
 
     return 0;
 }
